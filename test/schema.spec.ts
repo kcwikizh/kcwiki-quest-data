0d4829913b999d66a904c22daf4e8b5cf82126ad
lawvs <litusq@gmail.com>
2020-04-10 02:01:48 +0900

    feat: add json test

--- test/schema.spec.ts
+++ test/schema.spec.ts
@@ -4,21 +4,31 @@ import Ajv from 'ajv'
 import schemaDraft06 from 'ajv/lib/refs/json-schema-draft-06.json'
 
 import schema from '../build/quest-schema.json'
+import { loadAllJson } from '../scripts/utils'
+
+describe('Validating format', () => {
+  const DATA_DIR = 'data'
+  const files = fs.readdirSync(DATA_DIR)
+
+  it.each(files)(`%s match json object format`, (filename) => {
+    const file = fs.readFileSync(path.resolve(DATA_DIR, filename), 'utf8')
+    try {
+      const json = JSON.parse(file)
+      expect(typeof json).toBe('object')
+    } catch (error) {
+      fail('json parse error: ' + error)
+    }
+  })
+})
 
 describe('Validating schema', () => {
   const ajv = new Ajv()
   ajv.addMetaSchema(schemaDraft06)
   const validate = ajv.compile(schema)
 
-  const folder = 'data'
-  const files = fs.readdirSync(folder)
-
-  for (const filename of files) {
-    it(`${filename} match schema`, () => {
-      const file = fs.readFileSync(path.resolve('data', filename), 'utf8')
-      const json = JSON.parse(file)
-      const valid = validate(json)
-      expect(valid).toEqual(true)
-    })
-  }
+  const data = loadAllJson()
+  it.each(data)(`%p match schema`, (quest) => {
+    const valid = validate(quest)
+    expect(valid).toEqual(true)
+  })
 })
