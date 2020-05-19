c6a53b21bf83d42f1e27093dbc6f0eb6e3f5f3d4
lawvs <litusq@gmail.com>
2020-05-20 04:18:32 +0900

    feat: codegen translation

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Quest } from './types'
 import data from './build/data.json'
 
+export { translation } from './build/i18n'
 export * from './types'
 export const questData = data as Quest[]
