f9c540f39afd3d7f2864bb07d6ab2d6b1ee442eb
lawvs <litusq@gmail.com>
2021-01-19 20:42:47 +0900

    feat: gen kcanotify data ts file

--- scripts/download-kcanotify-gamedata.ts
+++ scripts/download-kcanotify-gamedata.ts
@@ -10,12 +10,13 @@ const URL_PREFIX =
 const VERSION_URL = `${URL_PREFIX}/DATA_VERSION`
 const DATA_URL = `${URL_PREFIX}/files`
 const LANGS = ['en', 'jp', 'scn', 'tcn'] as const
+const LOCALES = ['en-US', 'ja-JP', 'zh-CN', 'zh-TW'] as const
 
 if (!existsSync(OUTPUT_PATH)) {
   mkdirSync(OUTPUT_PATH)
 }
 
-const getDataVersion = async () => {
+const getRemoteVersion = async () => {
   const resp = await fetch(VERSION_URL)
   if (!resp.ok) {
     throw new Error(`Fetch Error!\nurl: ${resp.url}\nstatus: ${resp.status}`)
@@ -23,25 +24,53 @@ const getDataVersion = async () => {
   return resp.text()
 }
 
-const checkVersion = async () => {
-  const remoteVersion = await getDataVersion()
+const getLocalVersion = () => {
   const localVersionFile = path.resolve(OUTPUT_PATH, 'DATA_VERSION')
-  const localVersion = existsSync(localVersionFile)
+  const version = existsSync(localVersionFile)
     ? readFileSync(localVersionFile).toString()
-    : '-1'
-  if (remoteVersion === localVersion) {
-    console.log('The local version is up to date. Version: ', localVersion)
-    return false
-  }
-  console.log('New Version Detected. Version:', remoteVersion)
-  writeFileSync(`${OUTPUT_PATH}/DATA_VERSION`, remoteVersion)
-  return true
+    : '0'
+  return version
+}
+
+/**
+ * @example
+ * import zh_CN from './quests-scn.json'
+ * export default {
+ *   'zh-CN': zh_CN,
+ * }
+ * export const version = '5.1.2.1'
+ */
+const genTS = (version: string) => {
+  const importCode = LOCALES.map(
+    (locale, idx) =>
+      `import ${locale.replace('-', '_')} from './quests-${LANGS[idx]}.json'`,
+  ).join('\n')
+
+  const exportCode =
+    'export default {\n' +
+    LOCALES.map((locale) => `  '${locale}': ${locale.replace('-', '_')},`).join(
+      '\n',
+    ) +
+    '\n}'
+
+  const versionCode = `export const version = '${version}'`
+  return `${importCode}\n\n${exportCode}\n\n${versionCode}\n`
 }
 
 const main = async () => {
-  if (!(await checkVersion())) {
-    return
+  const args = process.argv.slice(2)
+
+  const remoteVersion = await getRemoteVersion()
+  const localVersion = getLocalVersion()
+  if (remoteVersion === localVersion) {
+    console.log('The local version is up to date. Version:', localVersion)
+    if (!args.find((v) => v === '-f' || v === '--force')) {
+      return
+    }
+  } else {
+    console.log('New Version Detected. Version:', remoteVersion)
   }
+  writeFileSync(`${OUTPUT_PATH}/DATA_VERSION`, remoteVersion)
 
   LANGS.forEach(async (lang) => {
     const filename = `quests-${lang}.json`
@@ -55,6 +84,8 @@ const main = async () => {
     }
     const data = await resp.text()
     writeFileSync(`${OUTPUT_PATH}/${filename}`, data)
+    const ts = genTS(remoteVersion)
+    writeFileSync(`${OUTPUT_PATH}/index.ts`, ts)
   })
 }
 
