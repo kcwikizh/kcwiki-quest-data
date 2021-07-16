0ef66faee41456670cbc85665312106798c6edc5
lawvs <lawvs@users.noreply.github.com>
2021-07-16 02:15:15 +0000

    deploy: 5e7880de20b5e262a75222b9ad623723a73ebb7d

---
+++ i18n/index.ts
@@ -0,0 +1,11 @@
+import zh_CN from './zh-CN.json'
+import zh_TW from './zh-TW.json'
+import ja_JP from './ja-JP.json'
+import en_US from './en-US.json'
+
+export default {
+  'zh-CN': zh_CN,
+  'zh-TW': zh_TW,
+  'ja-JP': ja_JP,
+  'en-US': en_US,
+}
