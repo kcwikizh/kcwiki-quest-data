4bb60dd3b1069191867cd8455a898ed898eb4c36
lawvs <lawvs@users.noreply.github.com>
2024-06-18 10:53:10 +0000

    deploy: f3a555a1326bf026fd5704d076ce145a7cdf8414

---
+++ kcanotify-gamedata/index.ts
@@ -0,0 +1,13 @@
+import en_US from './quests-en.json'
+import ja_JP from './quests-jp.json'
+import zh_CN from './quests-scn.json'
+import zh_TW from './quests-tcn.json'
+
+export default {
+  'en-US': en_US,
+  'ja-JP': ja_JP,
+  'zh-CN': zh_CN,
+  'zh-TW': zh_TW,
+}
+
+export const version = '5.8.7.0'
